aa843558203a16e09d3e0b1cf6adbe418ec8d069
Keveon Griffin <griffin.keveon22@gmail.com>
2024-01-01 00:09:56 -0500

    Created 'GlobalConfig' and 'IIDataConnection'

--- TrackerLibrary/IDataConnection.cs
+++ TrackerLibrary/IDataConnection.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
-    internal interface IDataConnection
+    public interface IDataConnection
     {
+        PrizeModel CreatePrize(PrizeModel model);
     }
 }
